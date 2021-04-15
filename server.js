ef0366dcd87ebb7ba0918d2422f88f6de7cdeb33
Tyler Johnson <tyler@csdconsultants.us>
2021-04-14 21:13:22 -0700

    completed cards 2 and 3 on trello

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
+//this creates the express server, much like importing in React
 const express = require('express');
-require ('dotenv').config();
 
+//this instantiates 
 const app = express();
 
+//this accesses the port varible from the .env file, OR uses 3001. REQUIRE DOTENV MUST BE ABOVE
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
-  response.send('hello');
+//connects or "imports" the .env file to use it's variables
+
+//This is proof of life
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+// // concludes express server setup
+
+
+const cors = require('cors');
+app.use(cors());
+
+const weatherData = require('./data/weather.json');
+
+
+app.get('/weather', (request, response) => {
+  try {
+    const allDailyForcasts = weatherData.data.map(day => new DailyForcast(day));
+    response.send(allDailyForcasts);
+  } catch (error) {
+    handleErrors(error, response);
+  }
 });
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+function DailyForcast(day) {
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
+
+function handleErrors(error, response) {
+  response.status(500).send('internal server error');
+}
